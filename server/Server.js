f91637c2aab2f5b607d71696cf91542e826f4ca7
hanumansaini12511 <er.hanumansaini@gmail.com>
2023-04-05 16:51:09 +0530

    ff

--- server/Server.js
+++ server/Server.js
@@ -3,16 +3,61 @@ const app = express()
 app.use(express.json())
 var bodyParser = require("body-parser");
 app.use(bodyParser.json());
+
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "mydb"
+});
+let data=''
+con.connect(function(err) {
+  
+  data="Connected!"
+});
+
 app.get("/",(req,res)=>{
-    res.json({ message: "Hello from server!" });
+    res.json({ message: data });
 })
 
-app.post("/insertdata",(req,res)=>{
+app.post("/createDb",(req,res)=>{
     let data={"name":req.body}
-    res.send({"name":req.body})
+    con.query("CREATE DATABASE "+req.body.db, function (err, result) {
+        if (err) throw err;
+        console.log("Database created");
+
+        data['connect']="Database created"
+      });
+      res.send(data)
+    
 })
+app.post("/createTable",(req,res)=>{
+    let data={"name":req.body}
+    var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Database created");
 
+        data['connect']="Database created"
+      });
+      res.send(data)
+    
+})
+app.post("/insertData",(req,res)=>{
+    let data={"name":req.body}
+var sql = "INSERT INTO customers (name, address) VALUES ('"+req.body.name+"','"+req.body.address+"' )";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Database created");
 
+        data['connect']="Database created"
+      });
+      res.send(data)
+    
+})
 var PORT = process.env.PORT || 8080;
 app.listen (PORT,(error)=>{
     if(!error){
