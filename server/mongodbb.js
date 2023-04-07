a13fcc3ac0f44be35d4db5e8d9f7eaf981206e17
hanumansaini12511 <er.hanumansaini@gmail.com>
2023-04-07 17:50:49 +0530

    node mongo

--- server/mongodbb.js
+++ server/mongodbb.js
@@ -4,19 +4,57 @@ app.use(express.json())
 var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb";
+
+/* const { MongoClient } = require('mongodb')
+
+// Create Instance of MongoClient for mongodb
+const client = new MongoClient('mongodb://0.0.0.0:27017') */
+
+
+ const mongoose = require('mongoose');
+mongoose.connect("mongodb://127.0.0.1:27017/db", {useNewUrlParser: true});
+const dogSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    breed: String
+   });
+
+   const Dog = mongoose.model("Dog", dogSchema);
+
+   /* Dog.find({}).then((user)=>{
+    console.log(user);
+   })  */
+
+ /* 
+   Dog.findOneAndDelete({name: 'hanu'}).then(()=>{console.log("deltee");})
+ */
+
+   Dog.findOneAndUpdate({ name: 'Rex' },{ name: 'hanu' }).then((d)=>{console.log("data"+d);})
+
+   Dog.find({}).then((user)=>{
+    console.log(user);
+   })  
+
+
+  /*  Dog.findOne({name:'hanu'}).then((user)=>{
+    console.log(user);
+   }) */
+
+
+
+  /*  const dog = new Dog({
+    name: "hanu",
+    age: 1,
+    breed: "Golden Retriever"
+   });
+   dog.save(); 
+   const docs =  Dog.find();
+   console.log(docs); */
 
 
 
 app.get("/",(req,res)=>{
-
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        console.log("Database created!");
-        db.close();
-      });
-    res.json({ message: "Database created!" });
+   
 })
 app.post("/createDb",(req,res)=>{
     let data={"name":req.body}
